1c492e630192e6ec3bbeb91f9dcef0953e17b9fc
jonathan5057 <GCAB022114@coderacademy.edu.au>
2022-03-14 20:10:22 +1000

    style: fix the footers file

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  # conditions - good, very good, and brand new
+  enum condition: {"good" => 1, "very good" => 2, "brand new" => 3}
 end
